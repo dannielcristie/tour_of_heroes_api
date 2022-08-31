6e9a064008b198ec3a096d4eb93686a509b589a8
Danniel Cristie <dany14012@gmail.com>
2022-08-31 19:09:12 -0300

    Altera CRUD para token no banco de dados

--- app/controllers/api/heroes_controller.rb
+++ app/controllers/api/heroes_controller.rb
@@ -3,12 +3,12 @@ module Api
   # HeroesController
   class HeroesController < ApplicationController
     include Authenticable
-    before_action :authenticate_with_token, except: %i[index show]
+    before_action :authenticate_with_token
     before_action :set_hero, only: %i[show update destroy]
 
     # GET /heroes
     def index
-      @heroes = Hero.search(params[:term]).sorted_by_name
+      @heroes = Hero.by_token(@token).search(params[:term]).sorted_by_name
 
       render json: @heroes
     end
@@ -20,7 +20,7 @@ module Api
 
     # POST /heroes
     def create
-      @hero = Hero.new(hero_params)
+      @hero = Hero.new(hero_params.to_h.merge!({ token: @token }))
 
       if @hero.save
         render json: @hero, status: :created, location: api_hero_url(@hero)
@@ -47,7 +47,7 @@ module Api
 
     # Use callbacks to share common setup or constraints between actions.
     def set_hero
-      @hero = Hero.find(params[:id])
+      @hero = Hero.by_token(@token).find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
--- app/models/hero.rb
+++ app/models/hero.rb
@@ -2,6 +2,7 @@
 class Hero < ApplicationRecord
   validates :name, :token, presence: true
 
-  scope :sorted_by_name, -> { order(:name) }
+  scope :by_token, ->(token) { where(token:) }
   scope :search, ->(term) { where('LOWER(name) LIKE ?', "%#{term.downcase}%") if term.present? }
+  scope :sorted_by_name, -> { order(:name) }
 end
