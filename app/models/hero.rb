222b811a3e9a7b6e599cd1eeca716dbe03940acb
Danniel Cristie <dany14012@gmail.com>
2022-09-01 12:11:28 -0300

    valida para hero name ser unico

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -1,6 +1,7 @@
 # Hero o
 class Hero < ApplicationRecord
   validates :name, :token, presence: true
+  validates :name, uniqueness: { scope: :token, case_sensitive: false }
 
   scope :by_token, ->(token) { where(token:) }
   scope :search, ->(term) { where('LOWER(name) LIKE ?', "%#{term.downcase}%") if term.present? }
