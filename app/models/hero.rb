1219411bc81525fd0b9feaa72613017c89ee3022
Danniel Cristie <dany14012@gmail.com>
2022-09-02 19:01:12 -0300

    add teste no model hero

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -1,7 +1,7 @@
 # Hero o
 class Hero < ApplicationRecord
-  validates :name, :token, presence: true
-  validates :name, uniqueness: { scope: :token, case_sensitive: false }
+  validates :name, presence: true, uniqueness: { scope: :token, case_sensitive: false }
+  validates :token, presence: true, length: { minimum: 10 }
 
   scope :by_token, ->(token) { where(token:) }
   scope :search, ->(term) { where('LOWER(name) LIKE ?', "%#{term.downcase}%") if term.present? }
